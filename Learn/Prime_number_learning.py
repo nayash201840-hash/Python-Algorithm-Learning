7b877b080a0b80d52d8b77853d056d363413628b
nayash201840-hash <nayash201840@gmail.com>
2026-09-25 00:47:20 +0530

    Update Prime_number_learning.py

--- Learn/Prime_number_learning.py
+++ Learn/Prime_number_learning.py
@@ -1,34 +1,47 @@
+def check_prime(number):
 
-def gcd_learning(a, b):
+    print("Checking number:", number)
 
-    if a < b:
-        a, b = b, a
+    if number <= 1:
+
+
+        print(number, "is NOT a prime number")
+
+        return False
+
+
+    square_root = int(number ** 0.5)
+
 
     print(f"""
-        Finding gcd {a} , {b} 
-        FACT: In Python, the remainder is calculated using the modulus operator '%'.
+    Square root of  {number} is {square_root}
+    FACT: Check the divisibility till the integer value of square root for the Prime Number.
     """)
 
-    step=1
+
+
+    for i in range(2, square_root + 1):
+
+        print("Checking:", number, "%", i)
+
+        if number % i == 0:
+
+            print(f"""
+            {number} is divisible by {i}
+            {number} is NOT a prime number.
+            """)
+
+            input("Press Enter to exit...")
+            return
+
+        print(number, "is NOT divisible by", i)
+
+        input("Press Enter to check the next number...")
+
+        
+
+    print(f"""
+    {number} is not divisible by {i}
+    {number} is  a PRIME number.
+    """)
     
-    while b != 0:
-
-        quotient = a // b
-        remainder = a % b
-
-        print("Step", step, ":", a, "=", b, "*", quotient, "+", remainder)
-
-
-
-        print("Here the remainder is:", remainder)
-
-        input("Press Enter for the next step...")
-
-        a = b
-        b = remainder
-
-        step += 1
-
-    print("Final result: gcd =", a)
-
-    return a
